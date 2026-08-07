0d08642a6e806a6bfed3d861873b6d6cdd9921ed
Tvman <elijahpauley702@gmail.com>
2026-08-07 18:10:44 -0400

    Create PhotonStubs.cs

---
+++ PhotonStubs.cs
@@ -0,0 +1,11 @@
+namespace Photon.Pun
+{
+    public class MonoBehaviourPunCallbacks {}
+    public class PhotonView { public static PhotonView Get(object obj) => null; }
+    public class PhotonNetwork { public static bool InRoom => false; public static void ConnectUsingSettings() {} }
+}
+
+namespace Photon.Realtime
+{
+    public class Player {}
+}
