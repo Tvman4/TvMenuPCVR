29bb0bac78689239f684e9ede97377569e707f46
Tvman <elijahpauley702@gmail.com>
2026-08-07 18:45:58 -0400

    Create PhotonStubs.cs

---
+++ PhotonStubs.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace Photon.Pun
+{
+    public class MonoBehaviourPunCallbacks : MonoBehaviour {}
+    public class PhotonView 
+    { 
+        public static PhotonView Get(object obj) => null; 
+    }
+    public class PhotonNetwork 
+    { 
+        public static bool InRoom => false; 
+        public static void ConnectUsingSettings() {} 
+        public static void Disconnect() {} 
+    }
+}
+
+namespace Photon.Realtime
+{
+    public class Player {}
+}
+
+namespace GorillaLocomotion
+{
+    public class Player : MonoBehaviour
+    {
+        public static Player Instance { get; private set; }
+        public float scale = 1f;
+        public float maxJumpSpeed = 6.5f;
+        public float jumpMultiplier = 1f;
+    }
+}
+
+public class GorillaPlayerScoreboardLine : MonoBehaviour
+{
+    public GameObject reportButton;
+}
