6979634c551a96df167b72ca7e5af1a1c9341958
Tvman <elijahpauley702@gmail.com>
2026-08-08 00:14:17 -0400

    Update PhotonStubs.cs

--- PhotonStubs.cs
+++ PhotonStubs.cs
@@ -2,22 +2,22 @@ using UnityEngine;
 
 namespace Photon.Pun
 {
-    public class MonoBehaviourPunCallbacks : MonoBehaviour {}
-    public class PhotonView 
-    { 
-        public static PhotonView Get(object obj) => null; 
+    public class MonoBehaviourPunCallbacks : MonoBehaviour { }
+    public class PhotonView
+    {
+        public static PhotonView Get(object obj) => null;
     }
-    public class PhotonNetwork 
-    { 
-        public static bool InRoom => false; 
-        public static void ConnectUsingSettings() {} 
-        public static void Disconnect() {} 
+    public class PhotonNetwork
+    {
+        public static bool InRoom => false;
+        public static void ConnectUsingSettings() { }
+        public static void Disconnect() { }
     }
 }
 
 namespace Photon.Realtime
 {
-    public class Player {}
+    public class Player { }
 }
 
 namespace GorillaLocomotion
@@ -25,10 +25,42 @@ namespace GorillaLocomotion
     public class Player : MonoBehaviour
     {
         public static Player Instance { get; private set; }
+
         public float scale = 1f;
         public float maxJumpSpeed = 6.5f;
         public float jumpMultiplier = 1f;
+
+        // Added so the menu compiles
+        public Transform leftHandTransform;
+        public Transform rightHandTransform;
+        public SphereCollider headCollider;
+        public CapsuleCollider bodyCollider;
+
+        public bool IsHandTouching(bool forLeftHand) => false;
     }
+
+    // Modern name used by newer game versions
+    public class GTPlayer : MonoBehaviour
+    {
+        public static GTPlayer Instance { get; private set; }
+
+        public float maxJumpSpeed = 6.5f;
+        public float jumpMultiplier = 1f;
+        public SphereCollider headCollider;
+
+        public bool IsHandTouching(bool isLeftHand) => false;
+    }
+}
+
+// Commonly used by almost every mod
+public class GorillaTagger : MonoBehaviour
+{
+    public static GorillaTagger Instance { get; private set; }
+
+    public Transform leftHandTransform;
+    public Transform rightHandTransform;
+    public SphereCollider headCollider;
+    public Transform transform; // just in case
 }
 
 public class GorillaPlayerScoreboardLine : MonoBehaviour
